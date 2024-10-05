0121cac676526c3383751d17bef1202963f91071
Priyaranjan <daspriyaranjan666@gmail.com>
2024-10-05 15:54:23 +0530

    first commit

---
+++ src/HRMS/TC001.java
@@ -0,0 +1,16 @@
+package HRMS;
+
+
+import org.sikuli.script.Screen;
+
+public class TC001 {
+	public static void main(String[] args) throws Exception {
+		Screen obj=new Screen();
+		obj.click("C:\\Users\\usar\\OneDrive\\Pictures\\sikuli\\Capture.PNG");
+        obj.doubleClick("C:\\Users\\usar\\OneDrive\\Pictures\\sikuli\\Cap.PNG");
+    	obj.click("C:\\Users\\usar\\OneDrive\\Pictures\\sikuli\\Ca.PNG");
+//		obj.doubleClick("C:\\Users\\usar\\Pictures\\sikuli ss\\ecli.PNG");
+//	
+	}
+
+}
